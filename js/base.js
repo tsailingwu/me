ed516b1e4c9fca188a55793aeab205514e0d3cc0
tsailingwu <nighytwinkle@gmail.com>
2017-03-14 14:17:27 +0800

    homepage update

--- js/base.js
+++ js/base.js
@@ -1,8 +1,25 @@
-// $(function (){
-// 	function detectWindow () {
-// 	var ww = $('.wrapper').width(),
-// 		wh = $(window).height(),
-// 		ratio = ww / wh;
-// 	};
-// 	console.log(ww / wh);
-// });
+$(function (){
+	function detectWindow() {
+		var ww = $('.wrapper').width(),
+			wh = $(window).height(),
+			ratio = ww / wh;
+			
+		console.log(ww);
+		console.log(wh);
+		console.log($('.wave').width());
+
+		$('.index .title').css('top', (wh - $('.index .title').height()) / 2);
+		$('.index').height(wh);
+		$('.wave').width(ww * 1.5).height(ww * 1.5);
+	}
+	detectWindow();
+	$(window).resize(function(){
+		detectWindow();
+	});
+
+	$('.arrow_down').click(function() {
+		$('html, body').animate({
+			scrollTop: $(window).height()
+		}, 800);
+	});
+});
